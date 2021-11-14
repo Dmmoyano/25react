746f319cfba17ff0291929d28f2b8d15080c72c0
aadriaan98 <adrianricardo123barros1@gmail.com>
2021-11-14 20:59:06 -0300

    ph

--- src/components/Form/Formulario.js
+++ src/components/Form/Formulario.js
@@ -1,6 +1,6 @@
 import React from "react";
 import {Form } from "react-bootstrap";
-
+import {senEmail} from "../../axios/Axios";
 
 
 export default function Formulario ({onChangeName, onChangeEmail, onChangePhone, onChangeMessage, onClick}){
