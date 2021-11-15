5620be5275d0c9ff571b1236133f873fff115d16
aadriaan98 <adrianricardo123barros1@gmail.com>
2021-11-14 22:30:05 -0300

    heroku

--- src/axios/Axios.js
+++ src/axios/Axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://dashboard.heroku.com/apps/back25watts"
+  baseURL: "https://front25watts.herokuapp.com/"
 })
 
 const sendEmail = async (dataConsole) => {
