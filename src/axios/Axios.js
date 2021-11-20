95ccb70146f315ddc69b453938ee57872b6bba42
Dmmoyano <moyanomartindiego@gmail.com>
2021-11-19 21:57:36 -0300

    api/public

--- src/axios/Axios.js
+++ src/axios/Axios.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "https://front25watts.herokuapp.com/"
+  baseURL: "https://pruebamundose.herokuapp.com/"
 })
 
 const sendEmail = async (dataConsole) => {
   const response = await instance({
     method: "post",
-    url: "api/probando",
+    url: "api/public",
     data: dataConsole,
   });
 
